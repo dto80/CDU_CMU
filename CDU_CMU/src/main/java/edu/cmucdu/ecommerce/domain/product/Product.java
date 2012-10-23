96fbeb2277779ad2b9d30824dd186b24d18b1b34
Satapat Muangsai <godsoneal@gmail.com>
2012-10-23 15:09:33 +0800

    Update add product form

--- CDU_CMU/src/main/java/edu/cmucdu/ecommerce/domain/product/Product.java
+++ CDU_CMU/src/main/java/edu/cmucdu/ecommerce/domain/product/Product.java
@@ -1,12 +1,16 @@
 package edu.cmucdu.ecommerce.domain.product;
 
 import edu.cmucdu.ecommerce.domain.util.Description;
+import edu.cmucdu.ecommerce.domain.util.LocaleEnum;
+
 import java.util.HashSet;
 import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.Transient;
+
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.entity.RooJpaEntity;
 import org.springframework.roo.addon.tostring.RooToString;
@@ -16,15 +20,17 @@ import org.springframework.roo.addon.tostring.RooToString;
 @RooJpaEntity
 public class Product {
 
-    @OneToOne
+    @OneToOne(cascade= CascadeType.ALL)
     private Description name;
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL)
     private Description description;
 
+    		
     @ManyToMany(cascade = CascadeType.ALL)
     private Set<SellerProduct> sellers = new HashSet<SellerProduct>();
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy="product")
     private Set<ProductPic> images = new HashSet<ProductPic>();
+
 }
--- CDU_CMU/src/main/java/edu/cmucdu/ecommerce/web/ProductController.java
+++ CDU_CMU/src/main/java/edu/cmucdu/ecommerce/web/ProductController.java
@@ -3,10 +3,13 @@ package edu.cmucdu.ecommerce.web;
 import edu.cmucdu.ecommerce.domain.product.Product;
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/products")
 @Controller
 @RooWebScaffold(path = "products", formBackingObject = Product.class)
 public class ProductController {
+	
 }
